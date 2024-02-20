f70a2fcbb60d8da0c1034edfd9f31c6f43c4cead
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-02-20 18:57:19 -0500

    added confirm for uppercase

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function generatePassword(){
 //Conditional Statement
 if (passwordSize>= 8 && passwordSize<=128) {
     var confirmLowerCase=confirm("Do you want lowercase included in the password?")
-    console.log(confirmLowerCase);
-
-    
+    console.log(confirmUpperCase);
+    var passwordSize=prompt("Enter password length between 8 and 128")
+    console.log(passwordSize)
+  
 } else {
   alert("Invalid Entry, Password size must be between 8 and 128.")
 }
