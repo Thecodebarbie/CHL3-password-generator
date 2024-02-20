df22fc7284c5a61246433cb1a771a949080257bd
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-02-20 18:12:47 -0500

    added prompt and conditional statment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var passwordSize=prompt("Enter password length between 8 and 128")
+  console.log(passwordSize)
+
+//Conditional Statement
+if (passwordSize>= 8 && passwordSize<=128) {
+    var confirmLowerCase=confirm("Do you want lowercase included in the password?")
+    console.log(confirmLowerCase);
+
+    
+} else {
+  alert("Invalid Entry, Password size must be between 8 and 128.")
+}
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
