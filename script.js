850afb2e43091c6e74e1d0bd0ff0dbb6a1216171
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-02-20 20:49:17 -0500

    created for loop and added conditional statements

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercase="abcdefjhijklmnopqrstuvwxyz";
-var uppercase=lowercase.toUpperCase();
-var numeric="0123456789";
-var specialCHaracters="`~!@#$%^&*()_+{}[]:";'<>?,./'
+var lowercase = "abcdefjhijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var numeric = "0123456789";
+var specialCHaracters = "`~!@#$%^&*()_+{}[]:"; '<>?,./'
 
 function generatePassword(){
-  var savedPasswords"";
-  var passwordSize=prompt("Enter password length between 8 and 128")
-  console.log(passwordSize)
-
-//Conditional Statement
-if (passwordSize>= 8 && passwordSize<=128) {
-    var confirmLowerCase=confirm("Do you want lowercase included in the password?")
-    console.log(confirmLowerCase);
-    var confirmUpperCase=confirm("Do you want uppercase included in the password?")
-    var confirmNumeric=confirm("Do you want numeric values included in the password?")
-    var confirmSpecialCharacters=confirm("Do you want numeric values included in the password?")
-} else {
-  alert("Invalid Entry, Password size must be between 8 and 128.")
+var savedPassword = "";
 }
 
+var passwordSize = prompt("Enter password length between 8 and 128")
+console.log(passwordSize)
+
+//Conditional Statement
+if (passwordSize >= 8 && passwordSize <= 128) {
+  var confirmLowerCase = confirm("Do you want lowercase included in the password?")
+  console.log(confirmLowerCase);
+  var confirmUpperCase = confirm("Do you want uppercase included in the password?")
+  var confirmNumeric = confirm("Do you want numeric values included in the password?")
+  var confirmSpecialCharacters = confirm("Do you want numeric values included in the password?");
 
 
-return savedPassword
+  for (var i = 0; i < passwordSize; i++) {
+
+    if (confirmLowerCase && savedPassword.length < passwordSize) {
+      var randomIndex = Math.floor(Math.random() * lowercase.length)
+      console.log(randomIndex)
+
+      savedPassword = savedPassword + lowercase[randomIndex]
+    }
+
+  }
+  if (confirmUpperCase && savedPassword.length < passwordSize) {
+    var randomIndex = Math.floor(Math.random() * uppercase.length);
+
+    console.log(randomIndex);
+
+    savedPassword = savedPassword + uppercase[randomIndex];
+
+    return savedPassword;
+  }
+  else {
+    alert("Invalid Entry, Password size must be between 8 and 128.");
+  }
 
 
 }
@@ -40,3 +59,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
